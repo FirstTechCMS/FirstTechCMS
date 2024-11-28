995b29bbd8dc9b3c73faabe449316fb2747b16eb
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-11-28 14:10:26 +0000

    Added telemetry debugging.

---
+++ src/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode_Linear.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.modules.DrivetrainController;
+
+@TeleOp(name="Main OpMode", group="Linear OpMode")
+public class MainOpMode_Linear extends LinearOpMode {
+    private DrivetrainController drivetrainController;
+    private void updateTelemetry(){
+        telemetry.addLine("================ Controls ================");
+        telemetry.addData("Gamepad 1", String.format("X: %f, Y:     %f", gamepad1.left_stick_x, gamepad1.left_stick_y));
+        telemetry.addData("Gamepad 2", String.format("X: %f, Y: %f", gamepad2.left_stick_x, gamepad2.left_stick_y));
+        telemetry.addLine("================ Motors ================");
+        telemetry.addData("Front Left", drivetrainController.frontLeftMotor.getPower());
+        telemetry.addData("Front Right", drivetrainController.frontRightMotor.getPower());
+        telemetry.addData("Back Left", drivetrainController.backLeftMotor.getPower());
+        telemetry.addData("Back Right", drivetrainController.backRightMotor.getPower());
+        telemetry.update();
+    }
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        drivetrainController = new DrivetrainController(hardwareMap);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double forward = -gamepad1.left_stick_y;
+            double right = gamepad1.left_stick_x;
+
+            drivetrainController.setMotors(forward + right, forward - right, forward + right, forward - right);
+
+            updateTelemetry();
+        }
+    }
+}
