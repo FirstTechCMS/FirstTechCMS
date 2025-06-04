6bf649a03085913168792dd955fb6c02ace919d3
jamesBouttell <19jbouttell@cms.tela.org.uk>
2025-06-04 08:52:47 +0100

    Changed autonomous mode.

--- src/FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode_Linear.java
+++ src/FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode_Linear.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.components.RobotComponentStore;
 import org.firstinspires.ftc.teamcode.components.interfaces.IArmComponent;
 import org.firstinspires.ftc.teamcode.components.interfaces.IDrivetrainComponent;
+import org.firstinspires.ftc.teamcode.components.interfaces.ISensorComponent;
 import org.firstinspires.ftc.teamcode.enums.OpModeId;
 import org.firstinspires.ftc.teamcode.math.Angle;
 
-@TeleOp(name = "Auto OpMode", group = "Linear OpMode")
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Autonomous(name = "Auto OpMode", group = "Linear OpMode")
 public class AutoOpMode_Linear extends LinearOpMode {
     @Override
     public void runOpMode() {
@@ -19,19 +24,67 @@ public class AutoOpMode_Linear extends LinearOpMode {
         RobotComponentStore.registerOpModeComponents(OpModeId.AUTONOMOUS, this);
 
         waitForStart();
+        timeOut(30000);
 
         IDrivetrainComponent drivetrain = RobotComponentStore.getComponent(IDrivetrainComponent.class);
         IArmComponent armComponent = RobotComponentStore.getComponent(IArmComponent.class);
-        armComponent.setAngle(Angle.fromTurns(0.5));
-        sleep(200);
-        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 1);
-        RobotComponentStore.update();
-        RobotComponentStore.updateTelemetry(telemetry);
-        sleep(2000);
+        ISensorComponent sensorComponent = RobotComponentStore.getComponent(ISensorComponent.class);
+
+        long startTime = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() - startTime < 20000) {
+            //Back up slightly
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), -0.2);
+            sleep(1000);
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
+
+            // Turns 90 degrees right
+            drivetrain.setTurnPower(0.5);
+            while (sensorComponent.getHeading().radians() < Angle.fromDegrees(90).radians()) {
+                RobotComponentStore.update();
+            }
+            drivetrain.setTurnPower(0);
+
+            // Moves forward until it is in the middle
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
+            while (sensorComponent.getDistanceBack() < 161) {
+                RobotComponentStore.update();
+                RobotComponentStore.updateTelemetry(telemetry);
+                sleep(100);
+            }
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
+
+            // Turns toward submersible
+            drivetrain.setTurnPower(0.5);
+            while (sensorComponent.getHeading().radians() < Angle.fromDegrees(180).radians()) {
+                RobotComponentStore.update();
+            }
+            drivetrain.setTurnPower(0);
+
+            // Move forward until it is right next to submersible
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
+            while (sensorComponent.getDistanceBack() < 75) {
+                RobotComponentStore.update();
+                RobotComponentStore.updateTelemetry(telemetry);
+                sleep(100);
+            }
+            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
+        }
 
         RobotComponentStore.update();
         RobotComponentStore.updateTelemetry(telemetry);
 
         telemetry.update();
     }
+
+    public void timeOut(int time) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                telemetry.addLine("Time limit reached, exiting Opmode");
+                System.exit(0);
+            }
+        }, time);
+    }
+
 }
