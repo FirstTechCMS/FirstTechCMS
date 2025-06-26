33e6ea578c46de099253e0305fe5f3c2b47a1e57
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2025-06-26 22:37:23 +0100

    Removed 20s loop. As per Ivan's request.

--- src/FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode_Linear.java
+++ src/FtcRobotController-10.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode_Linear.java
@@ -30,46 +30,42 @@ public class AutoOpMode_Linear extends LinearOpMode {
         IArmComponent armComponent = RobotComponentStore.getComponent(IArmComponent.class);
         ISensorComponent sensorComponent = RobotComponentStore.getComponent(ISensorComponent.class);
 
-        long startTime = System.currentTimeMillis();
+        //Back up slightly
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), -0.2);
+        sleep(1000);
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
 
-        while (System.currentTimeMillis() - startTime < 20000) {
-            //Back up slightly
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), -0.2);
-            sleep(1000);
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
-
-            // Turns 90 degrees right
-            drivetrain.setTurnPower(0.5);
-            while (sensorComponent.getHeading().radians() < Angle.fromDegrees(90).radians()) {
-                RobotComponentStore.update();
-            }
-            drivetrain.setTurnPower(0);
-
-            // Moves forward until it is in the middle
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
-            while (sensorComponent.getDistanceBack() < 161) {
-                RobotComponentStore.update();
-                RobotComponentStore.updateTelemetry(telemetry);
-                sleep(100);
-            }
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
-
-            // Turns toward submersible
-            drivetrain.setTurnPower(0.5);
-            while (sensorComponent.getHeading().radians() < Angle.fromDegrees(180).radians()) {
-                RobotComponentStore.update();
-            }
-            drivetrain.setTurnPower(0);
-
-            // Move forward until it is right next to submersible
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
-            while (sensorComponent.getDistanceBack() < 75) {
-                RobotComponentStore.update();
-                RobotComponentStore.updateTelemetry(telemetry);
-                sleep(100);
-            }
-            drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
+        // Turns 90 degrees right
+        drivetrain.setTurnPower(0.5);
+        while (sensorComponent.getHeading().radians() < Angle.fromDegrees(90).radians()) {
+            RobotComponentStore.update();
         }
+        drivetrain.setTurnPower(0);
+
+        // Moves forward until it is in the middle
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
+        while (sensorComponent.getDistanceBack() < 161) {
+            RobotComponentStore.update();
+            RobotComponentStore.updateTelemetry(telemetry);
+            sleep(100);
+        }
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
+
+        // Turns toward submersible
+        drivetrain.setTurnPower(0.5);
+        while (sensorComponent.getHeading().radians() < Angle.fromDegrees(180).radians()) {
+            RobotComponentStore.update();
+        }
+        drivetrain.setTurnPower(0);
+
+        // Move forward until it is right next to submersible
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0.2);
+        while (sensorComponent.getDistanceBack() < 75) {
+            RobotComponentStore.update();
+            RobotComponentStore.updateTelemetry(telemetry);
+            sleep(100);
+        }
+        drivetrain.setMecanumDrive(Angle.fromDegrees(0), 0);
 
         RobotComponentStore.update();
         RobotComponentStore.updateTelemetry(telemetry);
